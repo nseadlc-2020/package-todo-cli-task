6af4fc53cb11ef6b101ad07ce6a747dae3564868
Jacob Sherin <jacob@protoship.io>
2020-12-14 16:10:43 +0530

    remove usage from cpp code

--- cpp/todo.cpp
+++ cpp/todo.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 
-auto usage = R"(Usage :-
-$ ./todo add "todo item"  # Add a new todo
-$ ./todo ls               # Show remaining todos
-$ ./todo del NUMBER       # Delete a todo
-$ ./todo done NUMBER      # Complete a todo
-$ ./todo help             # Show usage
-$ ./todo report           # Statistics
-)";
+int main(int argc, char* argv[])
+{
 
-int main(int argc, char *argv[]) {
-
-  if (argc < 2) {
-    std::cout << usage;
-    return 0;
-  }
+    std::cout << "Hello, world!";
 }
